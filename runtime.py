273b1c8bff57aa56d4316875e1eac398c43b82d8
shisq <1215323732@qq.com>
2025-04-20 14:48:34 +0800

    init

--- runtime.py
+++ runtime.py
@@ -14,3 +14,13 @@ print(b)
 print(100 / 3) # 33.333
 
 print(100 // 3) # 33
+
+# 字符编码转换
+
+print(ord('A'), chr(66))
+
+b'ABC'.decode('ascii')
+b'\xe4\xb8\xad\xe6\x96\x87'.decode('utf-8')
+
+# 如果bytes中只有一小部分无效的字节，可以传入errors='ignore'忽略错误的字节：
+b'\xe4\xb8\xad\xff'.decode('utf-8', errors='ignore')
